a0f883bc926c9bde1dca27df123496741fa52bee
Po Wei Tsao <poweitsao@gmail.com>
2020-08-15 19:06:40 -0400

    started updating UI of subreddit podcast page

--- pages/subreddit/[subID].js
+++ pages/subreddit/[subID].js
@@ -20,8 +20,7 @@ import fetch from "isomorphic-unfetch"
 import { Icon, InlineIcon } from '@iconify/react';
 import playCircleOutlined from '@iconify/icons-ant-design/play-circle-outlined';
 import playCircleFilled from '@iconify/icons-ant-design/play-circle-filled';
-import { FullscreenExit } from '@material-ui/icons'
-
+import LaunchIcon from '@material-ui/icons/Launch';
 
 
 
@@ -77,34 +76,8 @@ const Subreddit = ({ userSession }) => {
 
 
     }, [subID])
+    console.log(playlist)
 
-    // const [AudioPlayerBar, setAudioPlayerBar] = useState(<AudioPlayerBar subreddit={subID} podcast={podcast} src={podcastURL} audio={} />)
-    const unlockAudio = async (track) => {
-        await track.play();
-        track.pause()
-        // if (playPromise !== undefined) {
-        //     await playPromise
-
-        //     playPromise.then(_ => {
-        //         // Automatic playback started!
-        //         // Show playing UI.
-        //         // We can now safely pause video...
-        //         console.log("pausing...")
-        //         track.pause();
-
-        //         // setAudio(track)
-
-        //     })
-        //         .catch(error => {
-        //             // Auto-play was prevented
-        //             // Show paused UI.
-        //             console.log("pausing...")
-        //             track.pause();
-        //         });
-
-
-        // }
-    }
     const playPodcast = async (podcast) => {
         const currStore = AudioPlayerStore.getState()
         if (currStore.url === podcast["cloud_storage_url"]) {
@@ -155,16 +128,16 @@ const Subreddit = ({ userSession }) => {
         return (
             <ListGroup.Item key={trackIndex}>
                 {/* <button onClick={() => { playPodcast(playlist[trackIndex]) }}>Play</button> */}
-                <div style={{ width: "250px" }}>
-                    <div style={{ fontSize: "16px", display: "flex", justifyContent: "center" }}>
+                <div style={{ width: "100%" }}>
+                    <div style={{ fontSize: "16px", display: "flex", justifyContent: "flex-start" }}>
                         <Icon
-                            style={{ width: "10%", height: "10%" }}
+                            style={{ width: "25px", height: "25px", marginRight: "20px" }}
                             icon={playButton}
                             onClick={() => { playPodcast(playlist[trackIndex]) }}
                             onMouseEnter={() => setPlayButton(playCircleFilled)}
                             onMouseLeave={() => setPlayButton(playCircleOutlined)} />
                         {/* <button onClick={() => { console.log(playlist) }}>Play</button> */}
-                        <div>{playlist[trackIndex]["filename"]}</div>
+                        <div style={{}}>{playlist[trackIndex]["filename"]}</div>
                     </div>
                 </div>
 
@@ -174,8 +147,11 @@ const Subreddit = ({ userSession }) => {
 
     const List = ({ playlist }) => {
         return (
-            <div>
-                <ListGroup >
+            <div style={{ width: "100%" }}>
+                <ListGroup variant="flush"></ListGroup>
+                <ListGroup variant="flush">
+                    <ListGroup.Item ><div style={{ paddingLeft: "45px" }}>Title</div></ListGroup.Item>
+
                     {playlist["keys"].map(renderTrack)}
                     {/* <ListGroup.Item>
                     <button onClick={() => { playPodcast(playlist[playlist["keys"][0]]) }}>Play</button> */}
@@ -183,7 +159,34 @@ const Subreddit = ({ userSession }) => {
 
                     {/* {playlist[playlist["keys"][0]]["filename"]} */}
                     {/* </ListGroup.Item> */}
+                    <ListGroup.Item></ListGroup.Item>
                 </ListGroup>
+            </div>
+        )
+    }
+
+    const SubredditInfo = (props) => {
+        return (
+            <div className="subreddit-info-container">
+                <Image className="album-cover" roundedCircle src={props.albumCover} width="256px" height="256px" />
+                <div className="subreddit-title">
+                    <h1>{"r/" + subID}</h1>
+                    <div>
+                        <a href={"https://www.reddit.com/r/" + subID} target="_blank">View on Reddit</a>
+                        <LaunchIcon style={{ height: "18px", width: "18px", paddingBottom: "3px" }} />
+                    </div>
+                </div>
+                <style>{`
+                .subreddit-info-container{
+                    display:flex;
+                    justify-content:space-between;
+                    align-items:center;
+                    padding: 20px;
+                }
+                .subreddit-title{
+                    padding:20px;
+                }
+            `}</style>
             </div>
         )
     }
@@ -199,7 +202,7 @@ const Subreddit = ({ userSession }) => {
             <div className="page-body">
                 {isEmpty(playlist)
                     ? <div></div>
-                    : <Image className="album-cover" src={playlist.album_cover_url} width="256px" height="256px" />}
+                    : <SubredditInfo albumCover={playlist.album_cover_url} />}
                 {isEmpty(playlist)
                     ? <div></div>
                     : <List playlist={playlist} />}
