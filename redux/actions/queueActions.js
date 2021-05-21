fc7e0c3a1fd6292e2103bb5e70e20a1962d552c3
Powei Tsao <poweitsao@gmail.com>
2021-05-21 01:58:47 -0400

    wrote queueActions and queueReducer

--- redux/actions/queueActions.js
+++ redux/actions/queueActions.js
@@ -4,3 +4,46 @@ const storeQueueInfo = (QueueInfo) => {
         QueueInfo
     }
 }
+
+const pushNextTrack = () => {
+    return {
+        type: "PUSH_NEXT_TRACK"
+    }
+}
+
+const addPlaylistToQueue = (newPlaylist) => {
+    return {
+        type: "ADD_PLAYLIST_TO_QUEUE",
+        newPlaylist
+    }
+}
+
+const clearCurrentPlaylist = () => {
+    return {
+        type: "CLEAR_CURRENT_PLAYLIST"
+    }
+}
+
+const removeTrackFromCurrentPlaylist = (trackID) => {
+    return {
+        type: "REMOVE_TRACK_FROM_CURRENT_PLAYLIST",
+        trackID
+    }
+}
+
+
+const removePlaylistFromQueue = (playlistID) => {
+    return {
+        type: "REMOVE_PLAYLIST_FROM_QUEUE",
+        playlistID
+    }
+}
+
+const removeTrackFromQueue = (trackID, index) => {
+    return {
+        type: "REMOVE_TRACK_FROM_QUEUE",
+        trackID,
+        index
+
+    }
+}
--- redux/reducers/QueueReducer.js
+++ redux/reducers/QueueReducer.js
@@ -1,4 +1,4 @@
-import { storeAudioPlayerInfo } from "../actions"
+import { nextTrack, storeAudioPlayerInfo } from "../actions"
 
 const initialState = {
     QueueInfo: {}
@@ -7,33 +7,69 @@ const initialState = {
 const queueInfoReducer = (state = initialState, action) => {
     // console.log("action:", action)
     switch (action.type) {
+        //! only called when putting db queue info into redux
         case "STORE_QUEUE_INFO":
             return Object.assign({}, state, {
-                // playing: action.AudioPlayerInfo.playing,
-                // subreddit: action.AudioPlayerInfo.subreddit,
-                // filename: action.AudioPlayerInfo.filename,
-                // trackName: action.AudioPlayerInfo.trackName,
-                // audio: action.AudioPlayerInfo.audio,
-                // url: action.AudioPlayerInfo.url,
-                // playlist: action.AudioPlayerInfo.playlist,
-                // keyIndex: action.AudioPlayerInfo.keyIndex
+                //? currentTrack = {trackID, trackName...}
+                currentTrack: action.QueueInfo.currentTrack,
+                //? currentPlaylist = {playlistID, playlistName, tracks[]...}
+                currentPlaylist: action.QueueInfo.currentPlaylist,
+                //? queue = [ {playlist1}, {playlist2}... ]
+                queue: action.QueueInfo.queue
             })
-        case "GET_NEXT_TRACK":
+        case "PUSH_NEXT_TRACK":
+            let currentPlaylist = state.currentPlaylist
+            let queue = state.queue
+            let nextTrack = {}
+            if (currentPlaylist.tracks.length == 0){
+                nextTrack = queue[0].tracks[0]
+                queue[0].tracks.shift()
+            } else{
+                nextTrack = currentPlaylist.tracks[0]
+                currentPlaylist.tracks.shift()
+            }
+            
             return {
-                ...state,
-                playing: action.playingStatus
+                currentTrack: nextTrack,
+                currentPlaylist: currentPlaylist,
+                queue: queue
             }
         case "ADD_PLAYLIST_TO_QUEUE":
+            let queue = state.queue
+            queue.unshift(action.newPlaylist)
             return {
-                ...state
+                ...state,
+                queue: queue
             }
-        case "DELETE_PLAYLIST_FROM_QUEUE":
+        case "CLEAR_CURRENT_PLAYLIST":
             return {
-                ...state
+                // ...state
             }
-        case "DELETE_TRACK_FROM_QUEUE":
+        case "REMOVE_TRACK_FROM_CURRENT_PLAYLIST":
             return {
-                ...state
+                // ...state
+            }
+
+        case "REMOVE_PLAYLIST_FROM_QUEUE":
+            let playlistID = action.playlistID
+            let queue = state.queue
+            queue = queue.filter((playlist) => {return playlist.playlistID != playlistID})
+            return {
+                ...state,
+                queue: queue
+
+            }
+        case "REMOVE_TRACK_FROM_QUEUE":
+            let trackID = action.trackID
+            let queue = state.queue
+            for (var i = 0; i < queue.length; i++){
+                let currQueuePlaylist = queue[i]
+                let filteredTracks = currQueuePlaylist.tracks.filter(
+                    (track) => {return track.trackID != trackID})
+                queue[i].tracks = filteredTracks
+            }
+            return {
+                // ...state
             }
         default:
             return state
