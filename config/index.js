eb76d25bdeede90b2021b0221068ea2442f74dc1
Powei Tsao <poweitsao@gmail.com>
2021-05-23 22:40:34 -0400

    updated production url to https://dev-po40361.vercel.app

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://snoopods.com';
+export const server = dev ? 'http://localhost:3000' : 'https://dev-po40361.vercel.app';
