3607a99e06c5af98d557cd55a57b8089785583af
Powei Tsao <poweitsao@gmail.com>
2021-06-01 15:53:41 -0400

    combinedReducers

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from 'redux';
+import AudioPlayerReducer from "./AudioPlayerReducer"
+import CollectionReducer from './CollectionReducer';
+import LikedTracksReducer from "./LikedTracksReducer"
+import QueueReducer from "./QueueReducer"
 import registerReducer from "./registerReducer"
+import SubListReducer from "./SubListReducer"
+import userSessionInfoReducer from "./userSessionInfoReducer"
 
-// const rootReducer = combineReducers({
-//     userInfo: registerReducer
-// });
 
-// export default rootReducer;
+const rootReducer = combineReducers({
+    audioPlayerInfo: AudioPlayerReducer,
+    collectionInfo: CollectionReducer,
+    likedTracksInfo: LikedTracksReducer,
+    queueInfo: QueueReducer,
+    registerInfo: registerReducer,
+    subListInfo: SubListReducer,
+    userSessionInfo: userSessionInfoReducer
+
+});
+
+export default rootReducer;
--- redux/store.js
+++ redux/store.js
@@ -1,37 +1,30 @@
 import { createStore } from 'redux';
-import registerReducer from "./reducers/registerReducer"
-import AudioPlayerReducer from "./reducers/AudioPlayerReducer"
-import QueueReducer from "./reducers/QueueReducer"
-import userSessionInfoReducer from "./reducers/userSessionInfoReducer"
-import collectionReducer from "./reducers/CollectionReducer"
-import LikedTracksReducer from './reducers/LikedTracksReducer';
-import subListReducer from './reducers/SubListReducer'
+import rootReducer from "./reducers/rootReducer"
+// import registerReducer from "./reducers/registerReducer"
+// import AudioPlayerReducer from "./reducers/AudioPlayerReducer"
+// import QueueReducer from "./reducers/QueueReducer"
+// import userSessionInfoReducer from "./reducers/userSessionInfoReducer"
+// import collectionReducer from "./reducers/CollectionReducer"
+// import LikedTracksReducer from './reducers/LikedTracksReducer';
+// import subListReducer from './reducers/SubListReducer'
 
-export const RegisterStore = createStore(registerReducer);
+// export const RegisterStore = createStore(registerReducer);
 
-export const AudioPlayerStore = createStore(AudioPlayerReducer);
+// export const AudioPlayerStore = createStore(AudioPlayerReducer);
 
-export const QueueStore = createStore(QueueReducer);
+// export const QueueStore = createStore(QueueReducer);
 
-export const UserSessionStore = createStore(userSessionInfoReducer);
+// export const UserSessionStore = createStore(userSessionInfoReducer);
 
-export const CollectionStore = createStore(collectionReducer)
+// export const CollectionStore = createStore(collectionReducer)
 
-export const LikedTracksStore = createStore(LikedTracksReducer)
+// export const LikedTracksStore = createStore(LikedTracksReducer)
 
-export const SubListStore = createStore(subListReducer)
+// export const SubListStore = createStore(subListReducer)
+const store = createStore(rootReducer)
 
 
-
-module.exports = {
-    RegisterStore,
-    AudioPlayerStore,
-    QueueStore,
-    UserSessionStore,
-    CollectionStore,
-    LikedTracksStore,
-    SubListStore
-};
+export default store
 
 // const store = createStore(rootReducer);
 
