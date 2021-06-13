b7248044c0544b57dc3bc9ff3350d3a5fa031363
Powei Tsao <poweitsao@gmail.com>
2021-06-13 10:55:14 -0400

    fixing gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# ListenToReddit-webserver
--- lib/convertDate.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from "../ts/interfaces";
-
-const convertDate = (dateObject: Timestamp) => {
-    var unixTime: Date = new Date(dateObject["_seconds"] * 1000);
-    var dateString: string = unixTime.toDateString();
-    return dateString.substring(4, 10) + ", " + dateString.substring(11, 15);
-  };
-
-export default convertDate
