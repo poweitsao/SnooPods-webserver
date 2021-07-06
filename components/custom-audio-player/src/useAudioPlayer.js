21af0c21ce1259515986b80b319de490b316ea5b
Powei Tsao <poweitsao@gmail.com>
2021-07-05 21:59:25 -0400

    WIP fixing bug for audio scrubbing. when mouse leaves bar it is buggy.

--- components/custom-audio-player/src/useAudioPlayer.js
+++ components/custom-audio-player/src/useAudioPlayer.js
@@ -8,6 +8,8 @@ function useAudioPlayer(audioObject, trackPlaying) {
   const [curTime, setCurTime] = useState();
   const [playing, setPlaying] = useState(trackPlaying);
   const [clickedTime, setClickedTime] = useState(-1);
+  const [currentlyScrubbing, setCurrentlyScrubbing] = useState(false)
+  const [syncBarTimeWithAudio, setSyncBarTimeWithAudio] = useState(false)
   const [curSource, setCurSource] = useState();
   const [source, setSource] = useState();
   const [audio, setAudio] = useState(audioObject);
@@ -23,17 +25,24 @@ function useAudioPlayer(audioObject, trackPlaying) {
       setCurSource(audio.source)
     }
 
-    const setAudioTime = () => setCurTime(audio.currentTime);
+
+    const setAudioTime = !currentlyScrubbing? () => setCurTime(audio.currentTime) : () => {};
 
     audio.addEventListener("loadeddata", setAudioData);
 
     audio.addEventListener("timeupdate", setAudioTime);
 
     // console.log("clickedTime", clickedTime)
-    if (clickedTime >= 0 && clickedTime !== curTime) {
-      console.log("curTime:", curTime)
-      audio.currentTime = clickedTime;
-      setClickedTime(-1);
+    if (clickedTime >= 0 && clickedTime !== curTime ) {
+      console.log("clickedTime:", clickedTime)
+      console.log("syncBarTimeWithAudio", syncBarTimeWithAudio  )
+      // window.addEventListener("mouseup", () => {setSyncBarTimeWithAudio(true)})
+      setCurTime(clickedTime)
+      if(syncBarTimeWithAudio){
+        audio.currentTime = clickedTime;
+        setSyncBarTimeWithAudio(false)
+        setClickedTime(-1);
+      }
     }
 
     if (curSource !== source) {
@@ -57,6 +66,9 @@ function useAudioPlayer(audioObject, trackPlaying) {
     setClickedTime,
     setSource,
     setCurTime,
+    setSyncBarTimeWithAudio,
+    setCurrentlyScrubbing,
+    currentlyScrubbing,
     curSource,
     source
   }
